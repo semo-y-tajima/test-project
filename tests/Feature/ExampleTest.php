f2e0c8c599b9ef9f04214c16e6251b635064d2c6
y-tajima <y-tajima@semo.co.jp>
2025-02-27 07:54:11 +0900

    テストサンプル

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,16 +4,31 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    use DatabaseTruncation;
 
-        $response->assertStatus(200);
+    public function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    public function test_database_truncation_works()
+    {
+        // ユーザーを作成
+        User::factory()->create(['email' => 'test@example.com']);
+
+        // データが1件あることを確認
+        $this->assertDatabaseHas('users', ['email' => 'test@example.com']);
+    }
+
+    public function test_database_is_empty()
+    {
+        // 上のテストが実行された後でも、データはクリアされているか確認
+        $this->assertDatabaseCount('users', 0);
     }
 }
